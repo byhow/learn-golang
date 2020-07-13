a03aae1a9b59ea2b0b86cb90052c12f1d8426473
duckino <25713361+duckino@users.noreply.github.com>
2020-07-13 17:41:04 -0500

    primitive types

--- README.md
+++ README.md
@@ -51,6 +51,51 @@ func main() {
   * Use strconv package for strings (or got unicode conversion)
   
 
-
-
-
+#### Primitive Types
+* Boolean 
+  * Values are true or false
+  * Not an alias for other types (e.g. int) 
+  * Zero value is false
+* Numeric 
+  * Integer
+    * Signed integers
+      * int types has varying size, but min 32 bits
+      * 8 bit (int8) through 64 bit (int64)
+    * Unsigned integers
+      * 8 bit (byte and uint8) through 32 bit (uint32)
+    * Arithmetic operations
+      * Addition, subtraction, multiplication, division, remainder
+    * Bitwise operations
+      * And, or, xor, and not
+    * Zero value is 0
+    * Cannot mix types in same family (uint16 + uint32 = error)
+  * Floating points
+    * Follow IEEE-754 standard
+    * Zero value is 0
+    * 32 and 64 bit versions
+    * Literal styles
+      * Decimal (3.14)
+      * Exponential (13e18 or 2e10)
+      * Mixed (13.7e2)
+    * Arithmetic operations
+      * Addition, subtraction, multiplication, division
+  * Complex numbers
+    * Zero value is (0 + 0i)
+    * 64 and 128 bit versions
+    * Built-in functions
+      * `complex` - make complex number from two floats
+      * `real` - get real part as float
+      * `imag` - get imaginary part as float
+    * Arithmetic operations
+      * Addition, subtraction, multiplication, division
+* Text types
+  * Strings
+    * UTF-8
+    * Immutable
+    * Can be concatenated with plus(+) operator
+    * Can be converted to []byte
+  * Rune
+    * UTF-32
+    * Alias for int32
+    * Special methods normally required to process
+      * e.g. strings.Reader#ReadRune
--- src/github.com/duckino/hello-world/Main.go
+++ src/github.com/duckino/hello-world/Main.go
@@ -34,4 +34,62 @@ func main() {
 	fmt.Printf(p + "\n")
 
 
+	// Primitives
+	var n bool = false
+	fmt.Printf("%v, %T\n", n, n)
+	
+	// m := 1 == 1
+	var y bool = 1 == 2
+	fmt.Printf("%v, %T\n", y, y)
+	// var q bool // if just initialize, then every value has a zero value, which to bool is false
+
+	// Numeric types
+	// int will at least be 32 bits
+	var w int16 = 42 // range to int64
+	fmt.Printf("%v, %T\n", w, w)
+	// var e uint16 = 42 // 0 - 65535, range to uint32
+
+	// arithmetic operation
+	fmt.Println(int(w) / k) // drop remainder
+	// only allows same type, very strict on type conversion
+
+	// Bitwise
+	a := 10 // 1010
+	b := 3 	// 0011
+	fmt.Println(a & b) // 0010
+	fmt.Println(a | b) // 1011
+	fmt.Println(a ^ b) // 1001 only one is 1
+	fmt.Println(a &^ b) // 0100 both 0
+
+	// Bit shifting
+	a = 8 // 2^3
+	fmt.Println(a << 3) // 2^3 * 2^3 = 2^6
+	fmt.Println(a >> 3) // 2^3 / 2^3 = 2^0
+
+	// Floating point numbers
+	na := 3.14
+	na = 4.7e72
+	na = 2.1E14
+	fmt.Printf("%v, %T\n", na, na)
+
+	// Complex number
+	var nb complex64 = 1 + 2i
+	fmt.Printf("%v, %T\n", real(nb), real(nb))
+	fmt.Printf("%v, %T\n", imag(nb), imag(nb))
+
+	var nb2 complex128 = complex(5, 12)
+	fmt.Printf("%v, %T\n", nb2, nb2)
+
+	// Text 
+	s := "this is a string"
+	// string is immutable
+	fmt.Printf("%v, %T\n", s[6], s[6])
+	// string concatenation is the same with other lang, +
+	s1 := []byte(s)
+	fmt.Printf("%v, %T\n", s1, s1) // got array of byte
+
+	// rune type: UTF-32
+	var r  rune = 'a' // use single quote
+	fmt.Printf("%v, %T\n", r, r) // will get int32 type, just type alias for int32
+	// we need this type for different apis, ReadRune vs ReadByte
 }
